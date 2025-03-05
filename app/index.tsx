df2666e869d00bcb21acea1f09a8c9fe0a191218
iLunZ <1040108307@qq.com>
2025-03-05 16:27:09 +0000

    feat: use camera

--- app/index.tsx
+++ app/index.tsx
@@ -1,60 +1,126 @@
-import { CameraView, CameraType, useCameraPermissions } from 'expo-camera';
-import { useState, useRef } from 'react';
-import { Button, StyleSheet, Text, View, Image, TouchableOpacity } from 'react-native';
+import {
+  CameraMode,
+  CameraType,
+  CameraView,
+  useCameraPermissions,
+} from "expo-camera";
+import { useRef, useState } from "react";
+import { Button, Pressable, StyleSheet, Text, View } from "react-native";
+import { Image } from "expo-image";
+import { AntDesign } from "@expo/vector-icons";
+import { Feather } from "@expo/vector-icons";
+import { FontAwesome6 } from "@expo/vector-icons";
 
-export default function Index() {
-  const camera = useRef<typeof CameraView | null>(null);
-  const [facing, setFacing] = useState<CameraType>('back');
-
+export default function App() {
   const [permission, requestPermission] = useCameraPermissions();
-  const [photo, setPhoto] = useState<string | null>(null);
+  const ref = useRef<CameraView>(null);
+  const [uri, setUri] = useState<string | null>(null);
+  const [mode, setMode] = useState<CameraMode>("picture");
+  const [facing, setFacing] = useState<CameraType>("back");
+  const [recording, setRecording] = useState(false);
 
   if (!permission) {
-    return <View />;
+    return null;
   }
 
   if (!permission.granted) {
     return (
       <View style={styles.container}>
-        <Text>We need your camera permission</Text>
-        <Button onPress={requestPermission} title="Grant Permission" />
+        <Text style={{ textAlign: "center" }}>
+          We need your permission to use the camera
+        </Text>
+        <Button onPress={requestPermission} title="Grant permission" />
       </View>
     );
   }
 
   const takePicture = async () => {
-    const photo = await camera.current?.takePictureAsync();
-    if (photo) {
-      setPhoto(photo.uri);
+    const photo = await ref.current?.takePictureAsync();
+    setUri(photo?.uri ?? null);
+  };
+
+  const recordVideo = async () => {
+    if (recording) {
+      setRecording(false);
+      ref.current?.stopRecording();
+      return;
     }
+    setRecording(true);
+    const video = await ref.current?.recordAsync();
+    console.log({ video });
+  };
+
+  const toggleMode = () => {
+    setMode((prev) => (prev === "picture" ? "video" : "picture"));
+  };
+
+  const toggleFacing = () => {
+    setFacing((prev) => (prev === "back" ? "front" : "back"));
+  };
+
+  const renderPicture = () => {
+    return (
+      <View>
+        <Image
+          source={{ uri }}
+          contentFit="contain"
+          style={{ width: 300, aspectRatio: 1 }}
+        />
+        <Button onPress={() => setUri(null)} title="Take another picture" />
+      </View>
+    );
+  };
+
+  const renderCamera = () => {
+    return (
+      <CameraView
+        style={styles.camera}
+        ref={ref}
+        mode={mode}
+        facing={facing}
+        mute={false}
+        responsiveOrientationWhenOrientationLocked
+      >
+        <View style={styles.shutterContainer}>
+          <Pressable onPress={toggleMode}>
+            {mode === "picture" ? (
+              <AntDesign name="picture" size={32} color="white" />
+            ) : (
+              <Feather name="video" size={32} color="white" />
+            )}
+          </Pressable>
+          <Pressable onPress={mode === "picture" ? takePicture : recordVideo}>
+            {({ pressed }) => (
+              <View
+                style={[
+                  styles.shutterBtn,
+                  {
+                    opacity: pressed ? 0.5 : 1,
+                  },
+                ]}
+              >
+                <View
+                  style={[
+                    styles.shutterBtnInner,
+                    {
+                      backgroundColor: mode === "picture" ? "white" : "red",
+                    },
+                  ]}
+                />
+              </View>
+            )}
+          </Pressable>
+          <Pressable onPress={toggleFacing}>
+            <FontAwesome6 name="rotate-left" size={32} color="white" />
+          </Pressable>
+        </View>
+      </CameraView>
+    );
   };
 
   return (
     <View style={styles.container}>
-      <View style={styles.cameraContainer}>
-        <CameraView 
-          style={styles.camera} 
-          facing={facing}
-        />
-      </View>
-      
-      <View style={styles.controlsContainer}>
-        <TouchableOpacity
-          style={styles.captureButton}
-          onPress={takePicture}
-        >
-          <Text style={styles.buttonText}>Take a photo</Text>
-        </TouchableOpacity>
-      </View>
-
-      {photo && (
-        <View style={styles.previewContainer}>
-          <Image
-            source={{ uri: photo }}
-            style={styles.preview}
-          />
-        </View>
-      )}
+      {uri ? renderPicture() : renderCamera()}
     </View>
   );
 }
@@ -62,40 +128,37 @@ export default function Index() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#000',
-  },
-  cameraContainer: {
-    flex: 2,
-    borderRadius: 20,
-    overflow: 'hidden',
-    marginTop: 40,
-    marginHorizontal: 20,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
   camera: {
     flex: 1,
+    width: "100%",
   },
-  controlsContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  shutterContainer: {
+    position: "absolute",
+    bottom: 44,
+    left: 0,
+    width: "100%",
+    alignItems: "center",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    paddingHorizontal: 30,
   },
-  captureButton: {
-    backgroundColor: 'white',
-    padding: 15,
-    borderRadius: 10,
-    marginTop: 20,
+  shutterBtn: {
+    backgroundColor: "transparent",
+    borderWidth: 5,
+    borderColor: "white",
+    width: 85,
+    height: 85,
+    borderRadius: 45,
+    alignItems: "center",
+    justifyContent: "center",
   },
-  buttonText: {
-    fontSize: 18,
-    color: 'black',
+  shutterBtnInner: {
+    width: 70,
+    height: 70,
+    borderRadius: 50,
   },
-  previewContainer: {
-    flex: 1,
-    padding: 20,
-  },
-  preview: {
-    width: '100%',
-    height: '100%',
-    borderRadius: 10,
-  }
 });
